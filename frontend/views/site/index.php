8231eaea8525fc77a20fd74b1c6920bd7678c680
Benjamin <Benjamin@Arbeitstier>
2016-05-09 11:34:50 +0200

    initial commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'InfoSys - Administrator';
+?>
+<div class="site-index">
+
+    <p>Hello World!</p>
+    <p>This is a Test.</p>
+    <p>Huehuehuehue</p>
+    
+</div>
